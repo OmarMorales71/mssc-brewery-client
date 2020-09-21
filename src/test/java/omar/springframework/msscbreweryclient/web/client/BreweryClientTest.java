b52eb4c729f5ca2a4dfa476f6a7bd487e9e5da53
Omar Morales <morales.ortega.omar@gmail.com>
2020-09-21 17:18:24 -0500

    adding post client method

--- src/test/java/omar/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/omar/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,5 +21,13 @@ class BreweryClientTest {
         BeerDto dto = client.getBeerById(UUID.randomUUID());
 
         assertNotNull(dto);
+    }
+    @Test
+    void saveNewBeer(){
+        BeerDto dto = BeerDto.builder().beerName("New Beer").build();
+
+        URI location = client.saveNewBeer(dto);
+
+        assertNotNull(location.toString());
     }
 }
